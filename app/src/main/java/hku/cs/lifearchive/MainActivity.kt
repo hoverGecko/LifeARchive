e50026f6c52069d02322c27ee15d91821b3372d1
Lee Cheuk Pui <bananapietrolllord@gmail.com>
2023-11-14 06:02:05 +0800

    Tried bottom nav view
    Next need inplement fragment

--- app/src/main/java/hku/cs/lifearchive/MainActivity.kt
+++ app/src/main/java/hku/cs/lifearchive/MainActivity.kt
@@ -2,10 +2,19 @@ package hku.cs.lifearchive
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomBar: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //init bottom app bar
+        bottomBar = findViewById(R.id.BottomBar)
+
+
+
+
     }
 }
