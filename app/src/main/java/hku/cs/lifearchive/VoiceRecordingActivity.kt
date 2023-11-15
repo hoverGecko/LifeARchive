8326070148ed6c3ea04410e12e6fc4345c4f3097
Henry Lee <hylee09.1173@gmail.com>
2023-11-16 00:21:34 +0800

    Added voice recording screen

---
+++ app/src/main/java/hku/cs/lifearchive/VoiceRecordingActivity.kt
@@ -0,0 +1,11 @@
+package hku.cs.lifearchive
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VoiceRecordingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_voice_recording)
+    }
+}
