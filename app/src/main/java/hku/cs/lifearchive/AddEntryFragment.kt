bc5dca064406219971c9c2def5a1cf207d062589
Lee Cheuk Pui <bananapietrolllord@gmail.com>
2023-11-25 07:29:15 +0800

    Add logic for navigation between add fragment

--- app/src/main/java/hku/cs/lifearchive/AddEntryFragment.kt
+++ app/src/main/java/hku/cs/lifearchive/AddEntryFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import hku.cs.lifearchive.diaryentrymodel.DiaryEntryDatabase
 
 class AddEntryFragment : Fragment() {
 
@@ -12,16 +14,39 @@ class AddEntryFragment : Fragment() {
         fun newInstance() = AddEntryFragment()
     }
 
-    private lateinit var viewModel: AddEntryViewModel
+
+
 
     override fun onCreateView(
+
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
+
+
         return inflater.inflate(R.layout.fragment_add_entry, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val diaryEntryDao = DiaryEntryDatabase.getDatabase(requireContext()).dao()
+        val button= view.findViewById<Button>(R.id.Add)
+        val title= view.findViewById<Button>(R.id.TitleInput)
+
+        button?.setOnClickListener {
+            title.editableText
+
+            println("button Clicked")
+            loadFragment(ItemFragment())
+        }
 
     }
 
-
-
+    private fun loadFragment(fragment: Fragment){
+        val transaction = activity?.supportFragmentManager?.beginTransaction()
+        transaction?.replace(this.id, fragment)
+        transaction?.commit()
+    }
 }
--- app/src/main/java/hku/cs/lifearchive/MainActivity.kt
+++ app/src/main/java/hku/cs/lifearchive/MainActivity.kt
@@ -1,6 +1,7 @@
 package hku.cs.lifearchive
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -49,7 +50,9 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+    fun AddandBack(v: View?){
 
+    }
 
     private fun loadFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction()
