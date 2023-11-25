dd8fad0d8d24a11cc3ff47261c36e6c564f2a8a6
Lee Cheuk Pui <bananapietrolllord@gmail.com>
2023-11-26 02:23:07 +0800

    Update AddEntryFragment.kt

--- app/src/main/java/hku/cs/lifearchive/AddEntryFragment.kt
+++ app/src/main/java/hku/cs/lifearchive/AddEntryFragment.kt
@@ -1,13 +1,18 @@
 package hku.cs.lifearchive
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.material.textfield.TextInputLayout
 import hku.cs.lifearchive.diaryentrymodel.DiaryEntry
 import hku.cs.lifearchive.diaryentrymodel.DiaryEntryDatabase
@@ -31,6 +36,7 @@ class AddEntryFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
 
         return inflater.inflate(R.layout.fragment_add_entry, container, false)
     }
@@ -48,6 +54,36 @@ class AddEntryFragment : Fragment() {
         nowdate.text = dates.toString()
 
         //TODO: find ways to get current longtitude and latitude
+        if (ActivityCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location : Location? ->
+
+                // Got last known location. In some rare situations this can be null.
+                if (location != null) {
+                    println("success")
+                    println(location.latitude)
+                    println(location.longitude)
+                }else
+                {
+                    println("null location")
+                }
+            }
 
 
         button?.setOnClickListener {
