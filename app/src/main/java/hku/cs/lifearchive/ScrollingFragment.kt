b14b918c90f33b908f96ca8832eb80309ba337a0
Lee Cheuk Pui <bananapietrolllord@gmail.com>
2023-11-26 22:33:53 +0800

    Added basci layout for scrolling view

--- app/src/main/java/hku/cs/lifearchive/ScrollingFragment.kt
+++ app/src/main/java/hku/cs/lifearchive/ScrollingFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import hku.cs.lifearchive.diaryentrymodel.DiaryEntryDatabase
 
 
 class ScrollingFragment : Fragment() {
@@ -18,12 +19,16 @@ class ScrollingFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_scrolling, container, false)
 
-        val idGet : TextView = view.findViewById(R.id.idview)
+        val titleGet : TextView = view.findViewById(R.id.Titledetail)
+        val contentGet: TextView = view.findViewById(R.id.Contentdetail)
         //get value from bundle
         val bundle = this.arguments
         if (bundle != null) {
             val id = bundle.getInt("id", defaultValue)
-            idGet.text = id.toString()
+            val diaryEntryDao = DiaryEntryDatabase.getDatabase(requireContext()).dao()
+            val target = diaryEntryDao.getById(id)
+            titleGet.text = target.title
+            contentGet.text = target.content
         }
         return view
     }
