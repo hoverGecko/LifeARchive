edc4d6a43f142b4d342a2ea5c86d0e551b39bc57
Lee Cheuk Pui <bananapietrolllord@gmail.com>
2023-11-27 02:32:32 +0800

    added ability to playback AR video

--- app/src/main/java/hku/cs/lifearchive/ScrollingFragment.kt
+++ app/src/main/java/hku/cs/lifearchive/ScrollingFragment.kt
@@ -1,10 +1,12 @@
 package hku.cs.lifearchive
 
 import android.R.attr.defaultValue
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import hku.cs.lifearchive.diaryentrymodel.DiaryEntryDatabase
@@ -21,6 +23,8 @@ class ScrollingFragment : Fragment() {
 
         val titleGet : TextView = view.findViewById(R.id.Titledetail)
         val contentGet: TextView = view.findViewById(R.id.Contentdetail)
+        val videotitle: TextView= view.findViewById(R.id.textView11)
+        val playbutton: Button = view.findViewById(R.id.Playvideo)
         //get value from bundle
         val bundle = this.arguments
         if (bundle != null) {
@@ -29,6 +33,36 @@ class ScrollingFragment : Fragment() {
             val target = diaryEntryDao.getById(id)
             titleGet.text = target.title
             contentGet.text = target.content
+
+            if(target.arVideoPath == null){
+                videotitle.visibility = View.GONE
+                playbutton.visibility = View.GONE
+            }else{
+                videotitle.visibility = View.VISIBLE
+                playbutton.visibility = View.VISIBLE
+                playbutton.setOnClickListener{
+                    val intent = Intent(activity, HelloRecordingPlaybackActivity::class.java)
+                    val bundle = Bundle()
+                    bundle.putString(
+                        HelloRecordingPlaybackActivity.DESIRED_APP_STATE_KEY,
+                        "PLAYBACK"
+                    )
+                    bundle.putString(
+                        HelloRecordingPlaybackActivity.DESIRED_DATASET_PATH_KEY,
+                        target.arVideoPath
+                    )
+                    bundle.putBoolean(
+                        "fromAddEntry",
+                        true
+                    )
+                    intent.putExtras(bundle)
+
+                    startActivity(intent)
+                    println("button Clicked")
+                }
+
+            }
+
         }
         return view
     }
