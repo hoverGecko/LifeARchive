27be07ff003e65b1c7aa633bfb61ad93469afbfb
Lee Cheuk Pui <bananapietrolllord@gmail.com>
2023-11-30 01:22:44 +0800

    Changed map fragment to map view

--- app/src/main/java/hku/cs/lifearchive/MapViewFragment.kt
+++ app/src/main/java/hku/cs/lifearchive/MapViewFragment.kt
@@ -6,51 +6,61 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import hku.cs.lifearchive.diaryentrymodel.DiaryEntryDatabase
 
 class MapViewFragment : Fragment() {
 
-    private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-
-        val diaryEntryDao = DiaryEntryDatabase.getDatabase(requireContext()).dao()
-        val allentry= diaryEntryDao.getAll()
-        for (entry in allentry){
-            val longs = entry.location?.longitude
-            val lats = entry.location?.latitude
-            val positions =  LatLng(lats!!, longs!!)
-
-            googleMap.addMarker(MarkerOptions().position(positions).title(entry.title))
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(positions, 10.0F))
-        }
-
-
-
-    }
-
+    private lateinit var googleMap: GoogleMap
+    private lateinit var mapper: MapView
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_maps, container, false)
+        return inflater.inflate(R.layout.fragment_maps123, container, false)
+    }
+    override fun onResume() {
+        super.onResume()
+        mapper.onResume()
     }
 
+    override fun onPause() {
+        super.onPause()
+        mapper.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapper.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapper.onLowMemory()
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        mapFragment?.getMapAsync(callback)
+        mapper =view.findViewById<MapView>(R.id.map2)
+        mapper.onCreate(savedInstanceState)
+        mapper.getMapAsync {
+            this.googleMap = it
+            val diaryEntryDao = DiaryEntryDatabase.getDatabase(requireContext()).dao()
+            val allentry = diaryEntryDao.getAll()
+            for (entry in allentry){
+                val longs = entry.location?.longitude
+                val lats = entry.location?.latitude
+                val positions =  LatLng(lats!!, longs!!)
+
+                googleMap.addMarker(MarkerOptions().position(positions).title(entry.title))
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(positions))
+            }
+
+        }
+
+
     }
 }
