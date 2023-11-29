5d5fc19dacbf3e767f6a4e335015b3ec1c47a15d
hoverGecko <u3593006@connect.hku.hk>
2023-11-30 02:46:12 +0800

    + Single photo support

--- app/src/main/java/hku/cs/lifearchive/MyItemRecyclerViewAdapter.kt
+++ app/src/main/java/hku/cs/lifearchive/MyItemRecyclerViewAdapter.kt
@@ -1,10 +1,14 @@
 package hku.cs.lifearchive
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
@@ -42,11 +46,7 @@ class MyItemRecyclerViewAdapter(
     override fun getItemCount(): Int = values.size
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = values[position]
-
-        holder.idView.text = item.title
-        //holder.contentView.text = item.content
-        holder.dateView.text = SimpleDateFormat("dd/MM/yyyy hh:mm:ss").format(item.date)
-        holder.detailview.setOnClickListener{
+        fun goToDetails(view: View, item: DiaryEntry) {
             //TODO: make a view fragment for it
             println("position")
             val bundle = Bundle()
@@ -60,15 +60,30 @@ class MyItemRecyclerViewAdapter(
                 ?.commit();
         }
 
+        holder.idView.text = item.title
+        //holder.contentView.text = item.content
+        holder.dateView.text = SimpleDateFormat("dd/MM/yyyy hh:mm").format(item.date)
+        holder.detailview.setOnClickListener {
+            goToDetails(it, item)
+        }
+        holder.card.setOnClickListener {
+            goToDetails(it, item)
+        }
 
-
-
+        println("picturePaths: ${item.picturePaths}")
+        if (item.picturePaths.isNotEmpty()) {
+            val photoUri = item.picturePaths[0]
+            holder.photoView.setImageURI(Uri.parse(photoUri))
+            holder.photoView.visibility = View.VISIBLE
+        }
     }
     inner class ViewHolder(binding: FragmentListViewEntryBinding) : RecyclerView.ViewHolder(binding.root) {
         val idView: TextView = binding.itemNumber
         //val contentView: TextView = binding.content
         val dateView: TextView = binding.date
         val detailview: Button = binding.entryViewBtn
+        val photoView: ImageView = binding.photoView
+        val card: LinearLayout = binding.root
     }
 
 
