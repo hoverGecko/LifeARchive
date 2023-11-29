0967039858b39b769ed353577702ea5de896ee41
hoverGecko <u3593006@connect.hku.hk>
2023-11-30 03:24:18 +0800

    Changed List view entry UI

--- app/src/main/java/hku/cs/lifearchive/MyItemRecyclerViewAdapter.kt
+++ app/src/main/java/hku/cs/lifearchive/MyItemRecyclerViewAdapter.kt
@@ -2,12 +2,10 @@ package hku.cs.lifearchive
 
 import android.content.Intent
 import android.net.Uri
-import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -15,6 +13,7 @@ import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
 import hku.cs.lifearchive.databinding.FragmentListViewEntryBinding
 import hku.cs.lifearchive.diaryentrymodel.DiaryEntry
+import org.w3c.dom.Text
 import java.text.SimpleDateFormat
 
 
@@ -54,10 +53,10 @@ class MyItemRecyclerViewAdapter(
             context1?.startActivity(intent)
         }
 
-        holder.idView.text = item.title
+        holder.titleView.text = item.title
         //holder.contentView.text = item.content
         holder.dateView.text = SimpleDateFormat("dd/MM/yyyy hh:mm").format(item.date)
-        holder.detailview.setOnClickListener {
+        holder.detailBtn.setOnClickListener {
             goToDetails(it, item)
         }
         holder.card.setOnClickListener {
@@ -69,13 +68,17 @@ class MyItemRecyclerViewAdapter(
             val photoUri = item.picturePaths[0]
             holder.photoView.setImageURI(Uri.parse(photoUri))
             holder.photoView.visibility = View.VISIBLE
+        } else if (!item.content.isNullOrBlank()) {
+            holder.contentView.text = item.content
+            holder.contentView.visibility = View.VISIBLE
         }
     }
     inner class ViewHolder(binding: FragmentListViewEntryBinding) : RecyclerView.ViewHolder(binding.root) {
-        val idView: TextView = binding.itemNumber
+        val titleView: TextView = binding.titleView
         //val contentView: TextView = binding.content
         val dateView: TextView = binding.date
-        val detailview: Button = binding.entryViewBtn
+        val detailBtn: Button = binding.entryViewBtn
+        val contentView: TextView = binding.contentView
         val photoView: ImageView = binding.photoView
         val card: LinearLayout = binding.root
     }
